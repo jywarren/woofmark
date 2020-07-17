028ce5f890dab3cc7bc2a8cb863a42e58364ef31
Shulammite-Aso <asoamarachi@gmail.com>
2020-07-17 13:05:10 +0000

    remove unrelated test

--- test/ui-tests/bold-module.test.js
+++ test/ui-tests/bold-module.test.js
@@ -6,29 +6,8 @@ beforeAll(async () => {
 });
 
 describe('bold module work as expected', () => {
-  test('retains bold selection state (on/off) after using new line', async () => {
-    await page.waitForSelector('.wk-wysiwyg');
-    await page.keyboard.type(' not using bold ', {delay: 160});
-    await page.keyboard.down('Control');
-    await page.keyboard.press('KeyB');
-    await page.keyboard.up('Control');
-    await page.keyboard.type('using bold ', {delay: 160});
-    await page.keyboard.down('Control');
-    await page.keyboard.press('KeyB');
-    await page.keyboard.up('Control');
-    await page.keyboard.press('Enter');
-    await page.keyboard.type('using new line', {delay: 160});
-
-    
-
-    const stringIsIncluded = await page.evaluate(() => document.querySelector('.wk-wysiwyg').innerHTML.includes('<strong>using new line</strong>'));
-    expect(stringIsIncluded).toBe(false);
-
-    
-  }, timeout);
 
   test('bold text does not loose formatting after converting to markdown and back to rich', async () => {
-    await page.goto('file://' + path, {waitUntil: 'domcontentloaded'});
     await page.waitForSelector('.wk-wysiwyg');
     await page.keyboard.type(' not using bold ', {delay: 160});
     await page.keyboard.down('Control');
